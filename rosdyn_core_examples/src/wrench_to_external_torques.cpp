c9fc8d3f26314d195502933d4db601a937e6fa48
Leonhard Euler <leonhard.euler@stiima.cnr.it>
2020-12-30 15:26:45 +0100

    wrench to torque

--- rosdyn_core_examples/src/wrench_to_external_torques.cpp
+++ rosdyn_core_examples/src/wrench_to_external_torques.cpp
@@ -8,9 +8,10 @@
 #include <rosdyn_core/frame_distance.h>
 
 #include <subscription_notifier/subscription_notifier.h>
+#include <rosdyn_core/spacevect_algebra.h>
 
 Eigen::VectorXd q;
-Eigen::VectorXd wrench;
+Eigen::VectorXd wrench_of_t_in_t;
 sensor_msgs::JointState model_js;
 sensor_msgs::JointState extra_js;
 
@@ -39,13 +40,13 @@ void jointStateCallback( const sensor_msgs::JointStateConstPtr& msg )
 
 void externalWrenchCallback( const geometry_msgs::WrenchStampedConstPtr& msg )
 {
-  wrench.resize(6);
-  wrench(0) = msg->wrench.force.x;
-  wrench(1) = msg->wrench.force.y;
-  wrench(2) = msg->wrench.force.z;
-  wrench(3) = msg->wrench.torque.x;
-  wrench(4) = msg->wrench.torque.y;
-  wrench(5) = msg->wrench.torque.z;
+  wrench_of_t_in_t.resize(6);
+  wrench_of_t_in_t(0) = msg->wrench.force.x;
+  wrench_of_t_in_t(1) = msg->wrench.force.y;
+  wrench_of_t_in_t(2) = msg->wrench.force.z;
+  wrench_of_t_in_t(3) = msg->wrench.torque.x;
+  wrench_of_t_in_t(4) = msg->wrench.torque.y;
+  wrench_of_t_in_t(5) = msg->wrench.torque.z;
 }
 
 int main(int argc, char **argv)
@@ -58,7 +59,7 @@ int main(int argc, char **argv)
   spinner.start();
   
   std::string base_frame = "ur5_base_link";
-  std::string tool_frame = "ur5_ee_link";                 //"ee_link";
+  std::string tool_frame = "ur5_tool0";                 //"ee_link";
   
   model_js.name.resize(6);
   model_js.position.clear();
@@ -99,33 +100,36 @@ int main(int argc, char **argv)
     {
       continue;
     }
-
-    Eigen::MatrixXd jacobian_of_a_in_b = chain->getJacobian( q );
-    Eigen::JacobiSVD<Eigen::MatrixXd> pinv_J(jacobian_of_a_in_b,  Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::Affine3d T_b_t = chain->getTransformation(q);
+    Eigen::MatrixXd jacobian_of_t_in_b = chain->getJacobian( q );
+    Eigen::JacobiSVD<Eigen::MatrixXd> pinv_J(jacobian_of_t_in_b,  Eigen::ComputeThinU | Eigen::ComputeThinV);
 
     double no_singularity=1;
     if ( (pinv_J.singularValues()(pinv_J.cols()-1)==0) || (pinv_J.singularValues()(0)/pinv_J.singularValues()(pinv_J.cols()-1) > 1e2))
     {
-      no_singularity=0;
+//       no_singularity=0;
 
       ROS_WARN_STREAM_THROTTLE(5,"SINGULARITY POINT (ellispoid deformed)");
       ROS_WARN_STREAM_THROTTLE(5,"  q        : " << q.transpose() );
-      ROS_WARN_STREAM_THROTTLE(5,"  det      : " << jacobian_of_a_in_b.determinant() );
+      ROS_WARN_STREAM_THROTTLE(5,"  det      : " << jacobian_of_t_in_b.determinant() );
       ROS_WARN_STREAM_THROTTLE(5,"  sin. val.: " << pinv_J.singularValues().transpose() );
 
     }
 
-    Eigen::VectorXd external_torque = 1 * jacobian_of_a_in_b.transpose() * wrench;
+    Eigen::VectorXd wrench_of_t_in_b = rosdyn::spatialRotation(wrench_of_t_in_t,T_b_t.linear());
+    Eigen::VectorXd external_torque = 1 * jacobian_of_t_in_b.transpose() * wrench_of_t_in_b;
 
     sensor_msgs::JointState external_torque_msg;
 
     external_torque_msg.name = model_js.name;
     external_torque_msg.effort.resize(model_js.name.size());
+    external_torque_msg.velocity.resize(model_js.name.size());
     external_torque_msg.position.resize(model_js.name.size());
     for(unsigned int i=0; i<external_torque_msg.effort.size();i++)
     {
       external_torque_msg.position.at(i) = q(i);
-      external_torque_msg.effort.at(i) = no_singularity*external_torque(i);
+      external_torque_msg.velocity.at(i) = 0.0;
+      external_torque_msg.effort.at(i)   = no_singularity*external_torque(i);
     }
 
     external_torque_pub.publish( external_torque_msg );
